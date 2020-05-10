IMPORTANT FIX. We used softmax twice, removed it

mode_tf.py
@@ -149,14 +149,14 @@ def main(namemodel,
             
             start_loss = tf.keras.losses.sparse_categorical_crossentropy(batch["start"], outputs["start"], from_logits=True)
             end_loss = tf.keras.losses.sparse_categorical_crossentropy(batch["end"], outputs["end"], from_logits=True)
-            long_loss = tf.keras.losses.sparse_categorical_crossentropy(batch["type"], outputs["long"], from_logits=True)
+            long_loss = tf.keras.losses.sparse_categorical_crossentropy(batch["long"], outputs["long"], from_logits=True)
             
             acc_1 = tf.keras.metrics.sparse_categorical_accuracy(
                 batch["start"], outputs["start"])          
             acc_2 = tf.keras.metrics.sparse_categorical_accuracy(
                 batch["end"], outputs["end"]) 
             acc_3 = tf.keras.metrics.sparse_categorical_accuracy(
-                batch["type"], outputs["long"])   
+                batch["long"], outputs["long"])   
 
             loss = ((tf.reduce_mean(start_loss) + tf.reduce_mean(end_loss) / 2.0) +
                 tf.reduce_mean(long_loss)) / 2.0

model_stuff/TFAlbertForNaturalQuestionAnswering.py
@@ -17,11 +17,11 @@ class TFAlbertForNaturalQuestionAnswering(TFAlbertPreTrainedModel):
 
         self.initializer = get_initializer(config.initializer_range)
         self.start = tf.keras.layers.Dense(1,
-            kernel_initializer=self.initializer, name='start', activation = "softmax")
+            kernel_initializer=self.initializer, name='start')
         self.end = tf.keras.layers.Dense(1,
-            kernel_initializer=self.initializer, name='end', activation = "softmax")
+            kernel_initializer=self.initializer, name='end')
         self.long_outputs = tf.keras.layers.Dense(1, kernel_initializer=self.initializer,
-            name='long', activation = "softmax")
+            name='long')
 
     def call(self, inputs, **kwargs):
         outputs = self.albert(inputs, **kwargs)